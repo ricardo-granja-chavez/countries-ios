7a07c0eda37a308a0609f89baef8d370defff551
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2022-11-29 23:47:10 -0600

    Creación de proyecto

---
+++ countries/countriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  countriesApp.swift
+//  countries
+//
+//  Created by Ricardo Granja Chávez on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct countriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
